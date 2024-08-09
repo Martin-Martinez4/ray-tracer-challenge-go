174d07b2d993ec61b898ac173cab326e1020ba0f
Martin <martmartez4@gmail.com>
2024-08-09 13:12:17 -0700

    Page 104

--- camera.go
+++ camera.go
@@ -1,1 +1,52 @@
 package main
+
+import "math"
+
+type Camera struct {
+	HSize       float64
+	VSize       float64
+	FieldOfView float64
+	Transform   Matrix4x4
+	HalfWidth   float64
+	HalfHeight  float64
+	PixelSize   float64
+}
+
+func PixelSize(camera *Camera) {
+	halfView := math.Tan(camera.FieldOfView / 2)
+	aspect := camera.HSize / camera.VSize
+
+	if aspect >= 1 {
+		camera.HalfWidth = halfView
+		camera.HalfHeight = halfView / float64(aspect)
+	} else {
+		camera.HalfWidth = halfView * float64(aspect)
+		camera.HalfHeight = halfView
+	}
+
+	camera.PixelSize = (camera.HalfWidth * 2) / float64(camera.HSize)
+
+}
+
+func NewCamera(hsize float64, vsize float64, fieldOfView float64) Camera {
+
+	tempCamera := Camera{HSize: hsize, VSize: vsize, FieldOfView: fieldOfView, Transform: IdentitiyMatrix4x4()}
+	PixelSize(&tempCamera)
+	return tempCamera
+
+}
+
+func RayForPixel(camera Camera, px, py float64) {
+	xoffset := (px + 0.5) * camera.PixelSize
+	yoffset := (py + 0.5) * camera.PixelSize
+
+	worldX := camera.HalfWidth - xoffset
+	worldY := camera.HalfHeight - yoffset
+
+	cameraTransformInv := camera.Transform.Inverse()
+	pixel := cameraTransformInv.TupleMultiply(Point(worldX, worldY, -1))
+	origin := cameraTransformInv.TupleMultiply(Point(0, 0, 0))
+	direction := Normalize(pixel.Subtract(origin))
+
+	return NewRay([3]float64{}, [3]float64{})
+}
--- camera_test.go
+++ camera_test.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCamera(T *testing.T) {
+
+	theWorld := NewDefaultWorld()
+
+	outer := theWorld.Spheres[0]
+	outer.Material.Ambient = 1
+	theWorld.Spheres[0] = outer
+
+	inner := theWorld.Spheres[1]
+	inner.Material.Ambient = 1
+	theWorld.Spheres[0] = inner
+
+	tests := []struct {
+		name  string
+		ray   Ray
+		world World
+		want  Color
+	}{
+		{
+			name:  "the color with an intersection behind the ray",
+			ray:   NewRay([3]float64{0, 0, 0.75}, [3]float64{0, 0, -1}),
+			world: theWorld,
+			want:  inner.Material.Color,
+		},
+	}
+
+	for i, tt := range tests {
+		T.Run(fmt.Sprintf("%d: %s", i, tt.name), func(t *testing.T) {
+
+			got := ColorAt(&tt.ray, &tt.world)
+
+			if !got.Equal(tt.want) {
+				t.Errorf("%d: \nwant: %v \ngot: %v \ndo not match", i, tt.want, got)
+			}
+
+		})
+	}
+}
